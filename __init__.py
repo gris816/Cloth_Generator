d473d5d7a7f377e039105482010f5f33aa3166e2
gris816 <gris816umi@gmail.com>
2025-05-15 22:47:07 +0900

    メッシュ生成オペレータの関数を別途定義

--- __init__.py
+++ __init__.py
@@ -13,16 +13,10 @@ import bmesh
 from bpy.types import Operator, Panel, PropertyGroup
 from bpy.props import StringProperty
 
-# オブジェクト生成ロジック
-class CLOTHICA_OT_generate_pattern(Operator):
-    bl_idname = "clothica.generate_pattern"
-    bl_label = "Generate Pattern"
-
-    def execute(self, context):
-        props = context.scene.clothica_pattern_props
-
-        pattern_shapes = {
-            'TOPS': [(-0.2475, 0.185, 0), (-0.2475, 0.105, 0), (-0.2475, -0.025, 0), (-0.2475, -0.185, 0),
+# メッシュ形状の定義関数
+def get_pattern_shape(pattern_type):
+    shapes = {
+        'TOPS': [(-0.2475, 0.185, 0), (-0.2475, 0.105, 0), (-0.2475, -0.025, 0), (-0.2475, -0.185, 0),
                     (-0.155, 0.105, 0),
                     (-0.0645, 0.185, 0),(-0.0645, 0.105, 0),(-0.0645, 0.035, 0),(-0.0645, -0.025, 0),
                     (-0.0075, -0.025, 0),(-0.0075, -0.185, 0),
@@ -31,44 +25,65 @@ class CLOTHICA_OT_generate_pattern(Operator):
                     (0.0765, 0.232, 0),(0.0765, -0.025, 0),
                     (0.2475, 0.232, 0),(0.2475, -0.025, 0),(0.2475, -0.185, 0)
                     ],
-            # ここからは仮の座標
-            'SLEEVE': [(-0.3, -0.2, 0), (0.3, -0.2, 0), (-0.2, 0.6, 0), (0.2, 0.6, 0)],
-            'SKIRT': [(-0.4, -0.6, 0), (0.4, -0.6, 0), (-0.2, 0.6, 0), (0.2, 0.6, 0)],
-            'PANTS_STRAIGHT': [(-0.3, -0.8, 0), (0.3, -0.8, 0), (-0.3, 0.8, 0), (0.3, 0.8, 0)],
-            'PANTS_SLIM': [(-0.2, -0.8, 0), (0.2, -0.8, 0), (-0.1, 0.8, 0), (0.1, 0.8, 0)],
+        'SLEEVE': [(-0.3, -0.2, 0), (0.3, -0.2, 0), (-0.2, 0.6, 0), (0.2, 0.6, 0)],
+        'SKIRT': [
+                      ],
+        'PANTS_STRAIGHT': [(-0.3, -0.8, 0), (0.3, -0.8, 0), (-0.3, 0.8, 0), (0.3, 0.8, 0)],
+        'PANTS_SLIM': [(-0.2, -0.8, 0), (0.2, -0.8, 0), (-0.1, 0.8, 0), (0.1, 0.8, 0)],
         }
-        
-        
-        verts = pattern_shapes.get(props.pattern_type, pattern_shapes['TOPS'])
+    return shapes.get(pattern_type, [])
 
-        mesh = bpy.data.meshes.new("mesh")
-        obj  = bpy.data.objects.new(f"{props.pattern_type}_Pattern", mesh)
+#面＋辺構成定義関数
+def get_faces_and_edges(pattern_type):
+    mapping = {
+        'TOPS': {
+            'faces': [(0, 1, 4, 6, 5), (1, 2, 8, 7, 6, 4), (2, 3, 10, 9, 8),
+                (9, 10, 18, 17, 15, 12), (14, 15, 17, 16),],
+            'edges': [(6, 8), (9, 7), (11, 13), (14, 16), (15, 17), (17, 18)]
+        },
+        'SLEEVE': {
+            'faces': [],
+            'edges': []
+        },
+        'SKIRT': {
+            'faces': [],
+            'edges': []
+        },
+        'PANTS_STRAIGHT': {
+            'faces': [],
+            'edges': []
+        },
+        'PANTS_SLIM': {
+            'faces': [],
+            'edges': []
+        },
+    }
+    return mapping.get(pattern_type, {'faces': [], 'edges': []})
+
+
+# メッシュ生成オペレータ
+class CLOTHICA_OT_generate_pattern(Operator):
+    bl_idname = "clothica.generate_pattern"
+    bl_label = "Generate Pattern"
+
+    pattern_type: StringProperty()
+
+    def execute(self, context):
+        verts = get_pattern_shape(self.pattern_type)
+        mesh = bpy.data.meshes.new(f"{self.pattern_type}_Mesh")
+        obj  = bpy.data.objects.new(f"{self.pattern_type}_Pattern", mesh)
         context.scene.collection.objects.link(obj)
 
         bm = bmesh.new()
         bm_verts = [bm.verts.new(v) for v in verts]
 
-        if props.pattern_type == 'TOPS':
-            # ポリゴンを手動で定義
-            faces = [
-                (0, 1, 4, 6, 5), (1, 2, 8, 7, 6, 4), (2, 3, 10, 9, 8),
-                (9, 10, 18, 17, 15, 12), (14, 15, 17, 16),
-            ]
-            for idxs in faces:
-                try:
-                    bm.faces.new([bm_verts[i] for i in idxs])
-                except:
-                    continue
-            edges_only = [(7, 11), (11, 13),(11, 12),]
-            for e in edges_only:
-                try:
-                    bm.edges.new([bm_verts[e[0]], bm_verts[e[1]]])
-                except:
-                    continue
-
-        else:
-            bm.faces.new([bm_verts[0], bm_verts[2], bm_verts[1]])
-            bm.faces.new([bm_verts[1], bm_verts[2], bm_verts[3]])
+        structure = get_faces_and_edges(self.pattern_type)
+        for f in structure['faces']:
+            try: bm.faces.new([bm_verts[i] for i in f])
+            except: continue
+        for e in structure['edges']:
+            try: bm.edges.new([bm_verts[e[0]], bm_verts[e[1]]])
+            except: continue      
 
         bm.to_mesh(mesh)
         bm.free()
